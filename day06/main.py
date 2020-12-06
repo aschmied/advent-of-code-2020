24d8eb99e5a8b8cf62ac8c77776ca919a43304cd
Anthony Schmieder <anthonyschmieder@nanometrics.ca>
2020-12-06 10:54:21 -0500

    Add day 6 solution

---
+++ day06/main.py
@@ -0,0 +1,37 @@
+def main():
+    with open('input') as f:
+        process_input(f)
+
+def process_input(iterable):
+    total_yes_answers = 0
+    total_yes_for_all_in_group = 0
+    for chunk in iterate_chunks(iterable):
+        yes_answers_by_traveller = [line.strip() for line in chunk]
+        total_yes_answers += count_unique_chars(''.join(yes_answers_by_traveller))
+        total_yes_for_all_in_group += len(intersection_of_chars(yes_answers_by_traveller))
+
+    print(f'Sum of total yes answer counts is {total_yes_answers}')
+    print(f'Sum of questions answered yes by everyone counts is {total_yes_for_all_in_group}')
+
+def count_unique_chars(string):
+    return len(set(list(string)))
+
+def intersection_of_chars(strings):
+    if len(strings) == 0:
+        return set()
+
+    intersection_of_chars = set(list(strings[0]))
+    return intersection_of_chars.intersection(*[set(list(s)) for s in strings])
+
+def iterate_chunks(iterable, delimiter='\n'):
+    elements_in_chunk = []
+    for element in iterable:
+        if element == delimiter:
+            yield elements_in_chunk
+            elements_in_chunk = []
+        else:
+            elements_in_chunk.append(element)
+    yield elements_in_chunk
+
+if __name__ == '__main__':
+    main()
---
+++ day06/test_main.py
@@ -0,0 +1,14 @@
+import unittest
+
+from main import intersection_of_chars
+
+class TestCountUnionOfChars(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(intersection_of_chars([]), set())
+
+    def test_one_string(self):
+        self.assertEqual(intersection_of_chars(['abc']), set(list('abc')))
+
+    def test_many_strings(self):
+        self.assertEqual(intersection_of_chars(['abc', 'ab', 'c']), set())
+        self.assertEqual(intersection_of_chars(['abc', 'ab', 'ac']), set('a'))
