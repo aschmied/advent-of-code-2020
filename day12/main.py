1768d8f474fedc9f832004feb5b0c1571e0304f2
Anthony Schmieder <anthonyschmieder@nanometrics.ca>
2020-12-12 11:49:09 -0500

    Add day 12 part 1

--- day12/main.py
+++ day12/main.py
@@ -1,7 +1,15 @@
 import enum
 
 def main():
-    pass
+    commands = read_movement_commands('input')
+    ship = Ship()
+    for command in commands:
+        command.execute(ship)
+    print(f'Manhattan distance from origin is {abs(ship.east()) + abs(ship.north())}')
+
+def read_movement_commands(filename):
+    with open(filename) as f:
+        return [parse_command(line.strip()) for line in f]
 
 class Ship:
     def __init__(self):
@@ -51,6 +59,65 @@ class Bearing(enum.Enum):
             return (-1, 0)
         elif self == Bearing.SOUTH:
             return (0, -1)
+
+    @classmethod
+    def parse(cls, string):
+        bearing_string_to_degrees = {
+            'E': 0,
+            'N': 90,
+            'W': 180,
+            'S': 270}
+        degrees = bearing_string_to_degrees[string]
+        return Bearing(degrees)
+
+def parse_command(command):
+    action = command[0]
+    argument = int(command[1:])
+
+    if action in ['E', 'N', 'W', 'S']:
+        return MoveCommand.build(action, argument)
+    elif action in ['L', 'R']:
+        return TurnCommand.build(action, argument)
+    elif action in ['F']:
+        return ForwardCommand.build(action, argument)
+    raise ValueError(f'Invalid command: {command}')
+
+class MoveCommand:
+    def __init__(self, bearing, distance):
+        self._bearing = bearing
+        self._distance = distance
+
+    def execute(self, ship):
+        ship.move(self._bearing, self._distance)
+
+    @classmethod
+    def build(cls, action, argument):
+        bearing = Bearing.parse(action)
+        return cls(bearing, argument)
+
+class TurnCommand:
+    def __init__(self, degrees):
+        self._degrees = degrees
+
+    def execute(self, ship):
+        ship.turn(self._degrees)
+
+    @classmethod
+    def build(cls, action, argument):
+        if action == 'R':
+            argument *= -1
+        return TurnCommand(argument)
+
+class ForwardCommand:
+    def __init__(self, distance):
+        self._distance = distance
+
+    def execute(self, ship):
+        ship.forward(self._distance)
+
+    @classmethod
+    def build(cls, action, argument):
+        return ForwardCommand(argument)
 
 if __name__ == '__main__':
     main()
